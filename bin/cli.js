cc9edb2218d2eb6fcff8d1f5942704acde22aa6d
I. Dan Calinescu <idancalinescu@gmail.com>
2016-11-01 11:27:41 +0200

    added more modular library code to be used for other commands

---
+++ bin/cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+var path  = require("path");
+var slana = require("slana");
+
+slana.run(path.dirname(__dirname));
--- exec/init.js
+++ exec/init.js
@@ -1,73 +1,18 @@
-var slana        = require("slana");
-var ejs          = require("ejs");
-var fs           = require("fs-extra");
-var path         = require("path");
-var cpy          = require('cpy');
-var recursive    = require("recursive-readdir");
 var config       = require("../config");
-
-function processTemplateFile(file, options) {
-  var templateContent  = fs.readFileSync(file, "utf8")
-  var templateCompiler = ejs.compile(templateContent)
-  var templateResult   = templateCompiler(options.context)
-
-  var targetFile       = options.relativeFile
-  var regex            = new RegExp('chunky', 'g');
-  targetFile           = path.join(options.dir, targetFile.replace(regex, options.name))
-
-  // First copy the file
-  fs.copySync(file, targetFile)
-
-  // Let's override its contents now
-  fs.writeFileSync(targetFile, templateResult, "utf8")
-}
-
-function createFromTemplate(name, options) {
-  var dir = path.resolve(path.join(config.templatesDir, name))
-
-  if (!fs.existsSync(dir)) {
-    throw new Error('Please specify a valid template')
-  }
-
-  var context = { name: options.name }
-  var targetDir = path.resolve(options.name)
-
-  if (fs.existsSync(targetDir)) {
-    // Make sure the host directory is clean
-    fs.removeSync(targetDir)
-  }
-
-  // Let's start with a fresh target
-  fs.mkdirSync(targetDir)
-
-  // First, copy the assets without parsing
-  cpy(config.assetsTypes, targetDir, { cwd: dir, parents: true }).then(() => {
-
-    // Next, fill in the templates
-    recursive(dir, config.templatesIgnores, function(err, files) {
-      files.map(function(file) {
-        var relativeFile = file.substring(dir.length + 1)
-        processTemplateFile(file, {
-          dir: path.resolve(options.name),
-          name: options.name,
-          targetFile: path.join(targetDir, relativeFile),
-          relativeFile,
-          context
-        })
-      })
-    });
-  });
-
-}
+var logger       = require("../lib/logger");
+var generator    = require("../lib/generator");
 
 function createWebApp(name) {
-  console.log("Creating React web app: " + name + " ...");
-  createFromTemplate("web/default", { name })
+  // logger.info("Creating React Web app", name)
+  // generator.createFromTemplate("web/default", { name })
+  // logger.done("Done")
+  throw new Error("Cannot create web apps - just yet. Stay tuned. Use --native to create a mobile app.")
 }
 
 function createMobileApp(name) {
-  console.log("Creating React Native app: " + name + " ...");
-  createFromTemplate("mobile/default", { name })
+  logger.info("Creating React Native app", name)
+  generator.createFromTemplate("mobile/default", { name })
+  logger.done("Done")
 }
 
 function parseCommand(command) {
@@ -90,6 +35,6 @@ module.exports = function(command) {
   try {
     parseCommand(command)
   } catch (error) {
-    slana.stopWithError(error)
+    logger.error(error)
   }
 }
